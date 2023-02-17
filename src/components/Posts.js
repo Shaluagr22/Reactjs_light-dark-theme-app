0c5d23f651400bf810591ae709009ea315e4a0df
Shalini <shalu.garg22@gmail.com>
2023-02-17 14:14:38 +0530

    First Commit

---
+++ src/components/Posts.js
@@ -0,0 +1,16 @@
+import React, { useContext} from 'react';
+import { ThemeContext } from '../context/themeContext';
+
+
+const Posts = () => {
+  const { theme, handleOnClick } = useContext(ThemeContext)
+  return (
+    <div>
+      <h4>My Post is {theme} in Color.</h4>
+      <button className={`btn ${theme === 'dark'? 'btn-light' : 'btn-dark'} border`} onClick={handleOnClick}
+      >{theme === "dark"? 'light':'dark'}</button>
+    </div>
+  )
+}
+
+export default Posts
